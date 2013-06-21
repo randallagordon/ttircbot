bcec7c38dfda3696f2b60f43eaa58dd49bb1edb0
Randall A. Gordon <randall@randallagordon.com>
2013-06-21 16:32:55 -0700

    Pipe Turntable song changes to IRC.

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 "use strict";
 
-var Bot  = require('ttapi');
-var repl = require('repl');
+var Bot    = require('ttapi');
+var repl   = require('repl');
+var irc    = require('irc');
 
-var AUTH = require('locals').AUTH;
+var AUTH   = require('locals').AUTH;
 var USERID = require('locals').USERID;
 var ROOMID = require('locals').ROOMID;
 
-var bot = new Bot(AUTH, USERID, ROOMID);
+var ttBot = new Bot(AUTH, USERID, ROOMID);
+repl.start('> ').context.ttBot = ttBot;
 
-repl.start('> ').context.bot = bot;
-
-bot.on('speak', function (data) {
-  // Respond to "/hello" command
-  if (data.text.match(/^\/hello$/)) {
-    bot.speak('Hey! How are you @'+data.name+' ?');
-  }
+var ircChannel = "#codescouts-inane";
+var ircBot = new irc.Client('irc.freenode.net', 'ranbot4000', {
+  channels: [ ircChannel ]
 });
 
-bot.on('newsong', function(songData) {
-  bot.getProfile( songData.room.metadata.current_dj, function(userData) {
-    console.log(userData.name + " started playing \"" + songData.room.metadata.current_song.metadata.song + "\" by " + songData.room.metadata.current_song.metadata.artist );
-    bot.bop();
+
+ttBot.on('newsong', function(songData) {
+  ttBot.getProfile( songData.room.metadata.current_dj, function(userData) {
+    var songChangeMsg = userData.name + " started playing \"" + songData.room.metadata.current_song.metadata.song + "\" by " + songData.room.metadata.current_song.metadata.artist;
+
+    // Say in channel
+    ircBot.say( ircChannel, songChangeMsg );
+
+    // Auto up vote love
+    ttBot.bop();
   });
 });
+
+ircBot.addListener('error', function(message) {
+  console.log('error: ', message);
+});
+
