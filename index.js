e1713992d68e21ab40a91560c2ad976c3c189245
Randall A. Gordon <randall@randallagordon.com>
2013-06-21 16:08:08 -0700

    Add REPL and log song change.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 "use strict";
 
+var Bot  = require('ttapi');
+var repl = require('repl');
+
 var AUTH = require('locals').AUTH;
 var USERID = require('locals').USERID;
 var ROOMID = require('locals').ROOMID;
 
-var Bot = require('ttapi');
 var bot = new Bot(AUTH, USERID, ROOMID);
+
+repl.start('> ').context.bot = bot;
 
 bot.on('speak', function (data) {
   // Respond to "/hello" command
   if (data.text.match(/^\/hello$/)) {
     bot.speak('Hey! How are you @'+data.name+' ?');
   }
+});
+
+bot.on('newsong', function(songData) {
+  bot.getProfile( songData.room.metadata.current_dj, function(userData) {
+    console.log(userData.name + " started playing \"" + songData.room.metadata.current_song.metadata.song + "\" by " + songData.room.metadata.current_song.metadata.artist );
+    bot.bop();
+  });
 });
