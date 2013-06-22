6bc58ca1e2bc3c845e6919ec35b0e65f85e8c658
Randall A. Gordon <randall@randallagordon.com>
2013-06-21 17:18:52 -0700

    Clean up code for Idiomatic.js adherence and comment thoroughly.

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 "use strict";
 
-var Bot    = require('ttapi');
-var repl   = require('repl');
-var irc    = require('irc');
+// require() necessary Node libraries
+var Bot    = require("ttapi");
+var irc    = require("irc");
 
-var AUTH   = require('locals').AUTH;
-var USERID = require('locals').USERID;
-var ROOMID = require('locals').ROOMID;
+// Pull in sensitive settings from locals.js
+var TTAUTH  = require("locals").TTAUTH;
+var TTUSER  = require("locals").TTUSER;
+var TTROOM  = require("locals").TTROOM;
+var IRCNET  = require("locals").IRCNET;
+var IRCNICK = require("locals").IRCNICK;
+var IRCCHAN = require("locals").IRCCHAN;
 
-var ttBot = new Bot(AUTH, USERID, ROOMID);
-repl.start('> ').context.ttBot = ttBot;
+// Start a REPL to interact with the bots
+var repl = require("repl").start( "> " );
 
-var ircChannel = "#codescouts-inane";
-var ircBot = new irc.Client('irc.freenode.net', 'ranbot4000', {
-  channels: [ ircChannel ]
+// Setup Turntable Bot
+var ttBot = new Bot( TTAUTH, TTUSER, TTROOM );
+repl.context.ttBot = ttBot;
+
+// Setup IRC Bot
+var ircBot = new irc.Client( IRCNET, IRCNICK, {
+  channels: [ IRCCHAN ]
 });
+repl.context.ircBot = ircBot;
 
-
-ttBot.on('newsong', function(songData) {
-  ttBot.getProfile( songData.room.metadata.current_dj, function(userData) {
+// When there's a new song, pipe it to IRC
+ttBot.on( "newsong", function( songData ) {
+  ttBot.getProfile( songData.room.metadata.current_dj, function( userData ) {
     var songChangeMsg = userData.name + " started playing \"" + songData.room.metadata.current_song.metadata.song + "\" by " + songData.room.metadata.current_song.metadata.artist;
 
     // Say in channel
-    ircBot.say( ircChannel, songChangeMsg );
+    ircBot.say( IRCCHAN, songChangeMsg );
 
     // Auto up vote love
     ttBot.bop();
   });
 });
 
-ircBot.addListener('error', function(message) {
-  console.log('error: ', message);
+// As per the node-irc README, this keeps the bot from crashing on IRC errors
+ircBot.addListener( "error", function( message ) {
+  console.log( "error: ", message );
 });
 
--- locals.js.ex
+++ locals.js.ex
@@ -1,6 +1,10 @@
 "use strict";
 
 // Use the Turntable-API bookmarklet and put your keys here
-module.exports.AUTH = '';
-module.exports.USERID = '';
-module.exports.ROOMID = '';
+module.exports.TTAUTH  = "";
+module.exports.TTUSER  = "";
+module.exports.TTROOM  = "";
+
+module.exports.IRCNET  = "";
+module.exports.IRCNICK = "";
+module.exports.IRCCHAN = "";
